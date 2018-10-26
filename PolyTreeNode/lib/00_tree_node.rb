86468b4a40a33048c8cfdfc8c7bc8c8a51d4c762
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-10-26 13:32:54 -0700

    Add PolyTreeNode class and methods

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,40 @@
 class PolyTreeNode
+  attr_reader :parent, :children, :value
+  # attr_accessor :parent
+
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
+
+  def parent=(parent_node)
+    # @parent = parent_node
+    # mom = self.parent
+    if parent_node == nil
+      @parent = nil
+      # mom.children.delete(self)
+      return
+    end
+
+    if !parent_node.already_have_child?(self)
+      update_relationship(parent_node)
+    end
+  end
+
+  def already_have_child?(child_node)
+    children.include?(child_node)
+  end
+
+  def update_relationship(parent_node)
+    if self.parent
+      old_parent = self.parent
+      old_parent.children.delete(self)
+    end
+
+    @parent = parent_node
+    parent_node.children.push(self)
+  end
+
 
 end
